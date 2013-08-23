84080f5c9efb5f1495af1d35e119e02a23e9fdd5
zachgersh <zdgman@gmail.com>
2013-08-22 20:42:23 -0500

    Finally auth'ed with github

--- lib/gist_nuke.rb
+++ lib/gist_nuke.rb
@@ -6,7 +6,7 @@ module GistNuke
   extend self
 
   BASE_URL = "https://api.github.com/"
-  AUTH_EXT = "/authorizations"
+  AUTH_EXT = "authorizations"
 
   def hi
     puts "hey"
@@ -21,14 +21,33 @@ module GistNuke
     credentials[:password] = begin `stty -echo` rescue nil
                  $stdin.gets.chomp
                ensure
-                 `stty -echo` rescue nil
+                 `stty echo` rescue nil
                end
 
+    puts ""
     phone_github(credentials)
   end
 
   def phone_github(credentials = {})
     uri = URI(BASE_URL + AUTH_EXT)
-    p uri
+    req = Net::HTTP::Post.new(uri.path)
+    req.content_type = "application/json"
+    req.body = JSON.dump({
+      scopes: [:gist],
+      note: "Batch delete your gists, you know you want to."
+    })
+
+    req.basic_auth(credentials[:username], credentials[:password])
+
+    res = Net::HTTP.start(uri.hostname, uri.port,
+                         :use_ssl => uri.scheme == 'https') do |http|
+      http.request(req)
+    end
+
+    puts res.body
+  end
+
+  def save_auth_token(token)
+
   end
 end
