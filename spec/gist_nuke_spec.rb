396048d88bcb2443b7e114496b5d36a9b86dabc2
zachgersh <zdgman@gmail.com>
2013-09-09 19:55:09 -0500

    fixes tests that were previosuly breaking

--- spec/gist_nuke_spec.rb
+++ spec/gist_nuke_spec.rb
@@ -3,7 +3,7 @@ require 'vcr_setup'
 
 describe GistNuke do
   around(:each) do |example|
-    VCR.use_cassette('github') do
+    VCR.use_cassette('github', :record => :once) do
       example.run
     end
   end
@@ -13,7 +13,7 @@ describe GistNuke do
       credentials = {username: "test", password: "test"}
 
       token = GistNuke.get_github_token(credentials)
-      token.should == "fe71e9391a26d6280ac377081d7ba3bfe2364993"
+      token.should == "5611d1d57d6837dedf584f0b9418da7cc8d7f976"
     end
   end
 
@@ -36,10 +36,12 @@ describe GistNuke do
 
   describe ".gather_pages" do
     it "should return an array of gists that is 34 keys long" do
+      GistNuke.stub(:token_from_file).and_return "fe71e9391a26d6280ac377081d7ba3bfe2364993"
       GistNuke.gather_pages(32..34).length == 34
     end
 
     it "should return an array of gists that is 30 keys long" do
+      GistNuke.stub(:token_from_file).and_return "fe71e9391a26d6280ac377081d7ba3bfe2364993"
       GistNuke.gather_pages(0..3).length == 30
     end
   end
--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -3,5 +3,4 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'vcr_cassettes'
   c.hook_into :webmock
-  c.default_cassette_options = { :record => :new_episodes, :erb => true }
 end
